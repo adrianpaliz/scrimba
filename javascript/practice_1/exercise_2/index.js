3cf77fa2fb90039af69e5952ed2868ff00ed29a9
adrianpaliz <adrianpaliz@gmail.com>
2022-10-07 22:48:18 -0500

    My solution for a function that logs out a string

---
+++ javascript/practice_1/exercise_2/index.js
@@ -0,0 +1,11 @@
+let name = "Amaru"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function userGreeting(){
+	greetingString = greeting + ", " + name + "!"
+	console.log(greetingString)
+}
+
+userGreeting()
