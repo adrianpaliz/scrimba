3faa22166ffbcd4ddb76017c2b73b4d1bf99d4be
adrianpaliz <adrianpaliz@gmail.com>
2022-10-17 06:07:23 -0500

    Add the message variable

--- javascript/blackjack_game/exercise_1/index.js
+++ javascript/blackjack_game/exercise_1/index.js
@@ -1,31 +1,21 @@
-console.log(4 === 3)  // false 
-console.log(5 > 2)    // true
-console.log(12 > 12)  // false
-console.log(3 < 0)    // false
-console.log(3 >= 3)   // true
-console.log(11 <= 11) // true
-console.log(3 <= 2)   // false
-
-
-/*
-let firstCard = 10 
+let firstCard = 11 
 let secondCard = 11
 
 let sum = firstCard + secondCard
 
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
 if (sum <= 20) {
-	console.log("Do you want to draw a new card? 🙂")
+	message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-	console.log("Wohoo! You've got Blackjack! 🥳")
+	message = "Wohoo! You've got Blackjack! 🥳"
 	hasBlackJack = true
 } else {
-	console.log("You're out of the game! 😭")
+	message = "You're out of the game! 😭"
 	isAlive = false
 }
 
-console.log(hasBlackJack)
-console.log(isAlive)
-*/
+console.log(message)
+
