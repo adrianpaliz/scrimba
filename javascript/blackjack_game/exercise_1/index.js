453c9c53bfc698ea5cbe61e86de939ec2a83fdc6
adrianpaliz <adrianpaliz@gmail.com>
2022-10-17 05:52:32 -0500

    Let's practice boolean conditions

--- javascript/blackjack_game/exercise_1/index.js
+++ javascript/blackjack_game/exercise_1/index.js
@@ -1,3 +1,13 @@
+console.log(4 === 3)  // false 
+console.log(5 > 2)    // true
+console.log(12 > 12)  // false
+console.log(3 < 0)    // false
+console.log(3 >= 3)   // true
+console.log(11 <= 11) // true
+console.log(3 <= 2)   // false
+
+
+/*
 let firstCard = 10 
 let secondCard = 11
 
@@ -18,4 +28,4 @@ if (sum <= 20) {
 
 console.log(hasBlackJack)
 console.log(isAlive)
-
+*/
