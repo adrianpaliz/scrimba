e3f88300a821fd9492d727876c13ac8ea9316182
adrianpaliz <adrianpaliz@gmail.com>
2022-10-20 23:01:21 -0500

    Array indexes

---
+++ javascript/blackjack_game/exercise_4/index.js
@@ -0,0 +1,11 @@
+let mySkills = [
+	"Gnu / Linux",
+	"Computer Maintenance",
+	"Content Management Systems"
+]
+
+console.log(mySkills[1])
+console.log(mySkills[2])
+console.log(mySkills[0])
+
+console.log( mySkills.length )
