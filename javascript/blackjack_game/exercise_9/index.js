0232c8434e6fc536d8d1ffb9e73991f4d7327035
adrianpaliz <adrianpaliz@gmail.com>
2022-10-21 00:33:12 -0500

    For loops and arrays

---
+++ javascript/blackjack_game/exercise_9/index.js
@@ -0,0 +1,12 @@
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+	"Great to hear",
+	"🙌"
+]
+
+for (let index = 0; index < messages.length; index += 1) {
+	console.log(messages[index])
+}
