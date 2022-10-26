49b280f69c68fba682d9daa789a16deab734d21f
adrianpaliz <adrianpaliz@gmail.com>
2022-10-25 22:39:32 -0500

    The logical OR operator

---
+++ javascript/blackjack_game/exercise_16/index.js
@@ -0,0 +1,12 @@
+let likesDocumentaries = true
+let likesStartups = false
+
+
+if (likesDocumentaries === true || likesStartups === true){
+	recommendMovie()
+}
+
+
+function recommendMovie() {
+	console.log("Hey, check out this new film we think will like!")
+}
