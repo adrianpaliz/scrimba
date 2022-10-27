341f7cb592f950e19f2b6fb62473530d35512d1b
adrianpaliz <adrianpaliz@gmail.com>
2022-10-27 02:19:18 -0500

    Teacher's solution to Rock papers scissors challenge

--- javascript/practice_2/exercise_6/index.js
+++ javascript/practice_2/exercise_6/index.js
@@ -1,15 +1,8 @@
 let hands = ["rock", "paper", "scissor"]
 
-let randomNumber = Math.floor( Math.random() * 3 )
-
-function randomItem() {
-	if (randomNumber === 0) {
-		return hands[0]
-	} else if (randomNumber === 1) {
-		return hands[1]
-	} else {
-		return hands[2]
-	}
+function getRandomItem() {
+	let randomIndex = Math.floor( Math.random() * 3 )
+	return hands[randomIndex]
 }
 
-console.log(randomItem())
+console.log(getRandomItem())
