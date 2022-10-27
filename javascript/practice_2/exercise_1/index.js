be2740665aae7d993924bcb2c155855843dd889f
adrianpaliz <adrianpaliz@gmail.com>
2022-10-26 22:41:59 -0500

    Objects and functions

---
+++ javascript/practice_2/exercise_1/index.js
@@ -0,0 +1,12 @@
+let humanBeing = {
+	name: "Adrián",
+	age: 32,
+	country: "Ecuador"
+}
+
+function logData() {
+	string = humanBeing.name + " is " + humanBeing.age + " years old and lives in " + humanBeing.country
+	console.log( string )
+}
+
+logData()
