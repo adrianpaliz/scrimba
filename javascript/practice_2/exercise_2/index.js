50e3a957399236106d6b28bb5c4ac29e51f35afe
adrianpaliz <adrianpaliz@gmail.com>
2022-10-26 23:11:34 -0500

    My solution to the challenge

---
+++ javascript/practice_2/exercise_2/index.js
@@ -0,0 +1,14 @@
+let age = 18
+
+
+if ( age <= 6 ){
+	console.log("Free")
+} else if ( age >= 6 && age <= 17){
+	console.log("Child discount")
+} else if ( age >= 18 && age <= 26){
+	console.log("Student discount")
+} else if ( age >= 27 && age <= 66){
+	console.log("Full price")
+} else {
+	console.log("Senior citizen discount")
+}
