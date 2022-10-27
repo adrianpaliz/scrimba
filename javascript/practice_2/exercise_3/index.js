64c17cd39e6e0127891eae1d7c829092b3326db2
adrianpaliz <adrianpaliz@gmail.com>
2022-10-27 00:40:38 -0500

    My solution to the challenge

---
+++ javascript/practice_2/exercise_3/index.js
@@ -0,0 +1,7 @@
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
+
+console.log("The 5 largest countries in the world:")
+
+for (let index = 0; index < 5; index +=1) {
+	console.log("- " + largeCountries[index])
+}
