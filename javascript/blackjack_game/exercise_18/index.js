d3e9fad166d1f0840b6b9dce9f5eb0f5e1771d34
adrianpaliz <adrianpaliz@gmail.com>
2022-10-26 21:59:26 -0500

    Create my first object

---
+++ javascript/blackjack_game/exercise_18/index.js
@@ -0,0 +1,12 @@
+let home = {
+	title: "Treehouse. Suite, 2 people. Mindo.",
+	price: 55,
+	reviews: 28,
+	available: true,
+	facilities: ["Garden view", "Wifi", "Free parking"],
+}
+
+console.log( home.price )
+console.log( home.facilities )
+console.log( home.available )
+
