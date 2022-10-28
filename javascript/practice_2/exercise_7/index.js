1ba021acbfcbae6139493078181f157cbb7ad716
adrianpaliz <adrianpaliz@gmail.com>
2022-10-27 23:25:55 -0500

    Change to use string length property, based on teacher's solution

--- javascript/practice_2/exercise_7/index.js
+++ javascript/practice_2/exercise_7/index.js
@@ -4,7 +4,7 @@ let stageElement = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
-    let randomIndexOne = Math.floor( Math.random() * 17 )
-	let randomIndexTwo = Math.floor( Math.random() * 17 ) 
+    let randomIndexOne = Math.floor( Math.random() * fighters.length )
+	let randomIndexTwo = Math.floor( Math.random() * fighters.length ) 
 	stageElement.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
 })
