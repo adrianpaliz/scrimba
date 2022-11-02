6277d7295feca83398c82e29bcf260c24c40cc80
adrianpaliz <adrianpaliz@gmail.com>
2022-11-01 23:39:38 -0500

    Define passwordLength, for loop added for generate a randomPassword using getRandomCharacter function, and use value property to return the value in the input

--- javascript/practice_2/project_password_generator/index.js
+++ javascript/practice_2/project_password_generator/index.js
@@ -1,11 +1,22 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordFieldOneElement = document.getElementById("passwordFieldOne")
 
-let generateButton = document.getElementById("generateButton")
+let passwordFieldOneElement = document.getElementById("passwordFieldOne")
+let generateButton = document.getElementById("generateButton") 
+let passwordLength = 12
+
+
+function getRandomCharacter() {
+	let randomIndex = Math.floor(Math.random() * characters.length)
+	return characters[randomIndex]
+}
 
 generateButton.addEventListener("click", function() {
-	let randomIndex = Math.floor( Math.random() * characters.length )
-	passwordFieldOneElement.value = characters[randomIndex]
-	})
+	let randomPassword = ""
+	for(let index = 0; index < passwordLength; index++){
+		randomPassword += getRandomCharacter()
+	passwordFieldOneElement.value = randomPassword
+	}
+})
+ 
