69d2ba65478529758cd58c646c80eeb2d807f240
adrianpaliz <adrianpaliz@gmail.com>
2022-11-24 22:50:10 -0500

    Create the myLeads array and inputElement constant

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -1,4 +1,6 @@
-let inputButtom = document.getElementById("input-button")
+const inputButtom = document.getElementById("input-button")
+let myLeads =[]
+const inputElement = document.getElementById("input-element")
 
 inputButtom.addEventListener("click", function() {
 	console.log("Button clicked from addEventListener")
