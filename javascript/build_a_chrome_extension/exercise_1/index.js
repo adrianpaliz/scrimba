a9e5aa2f5c35b4b400e261cc6631d0a4fd313af5
adrianpaliz <adrianpaliz@gmail.com>
2024-04-09 20:46:49 -0500

    Push a hardcoded lead to myLeads array. Also code lines were rearrange

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -1,7 +1,9 @@
-const inputButtom = document.getElementById("input-button")
 let myLeads =[]
+
 const inputElement = document.getElementById("input-element")
+const inputButtom = document.getElementById("input-button")
 
 inputButtom.addEventListener("click", function() {
-	console.log("Button clicked from addEventListener")
+	myLeads.push("www.awesomelead.com")
+	console.log(myLeads)
 })
