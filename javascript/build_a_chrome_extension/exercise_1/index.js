d5a50dba02915f63efcf566964a6371e8ccb42cb
adrianpaliz <adrianpaliz@gmail.com>
2024-04-11 12:58:35 -0500

    Use a for loop to log out leads

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -1,4 +1,4 @@
-let myLeads =[]
+let myLeads =["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 const inputElement = document.getElementById("input-element")
 const inputButtom = document.getElementById("input-button")
@@ -7,3 +7,7 @@ inputButtom.addEventListener("click", function() {
 	myLeads.push(inputElement.value)
 	console.log(myLeads)
 })
+
+for (let index = 0; index < myLeads.length; index++) {	
+	console.log(myLeads[index]) 
+}
