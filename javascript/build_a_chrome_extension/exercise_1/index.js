ffad5c0644d46674df6d55f1e6e4cb75fcbcb289
adrianpaliz <adrianpaliz@gmail.com>
2024-04-13 00:29:59 -0500

    A variable was created and was assigned to a empty string, iten added to that variable instead of using innerHTML property and the list was rendered

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -3,6 +3,7 @@ let myLeads =["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputElement = document.getElementById("input-element")
 const inputButtom = document.getElementById("input-button")
 const unorderedListElement = document.getElementById("unordered-list-element")
+let listItems = "" 
 
 inputButtom.addEventListener("click", function() {
 	myLeads.push(inputElement.value)
@@ -10,11 +11,7 @@ inputButtom.addEventListener("click", function() {
 })
 
 for (let index = 0; index < myLeads.length; index++) {	
-	// unorderedListElement.innerHTML += "<li>" + myLeads[index] + "</li>"
-	// create element
-	// set text content
-	// append to unorderedListElement
-	const liElement = document.createElement("li")
-	liElement.textContent = myLeads[index]
-	unorderedListElement.append(liElement)
+	listItems += "<li>" + myLeads[index] + "</li>"
+	console.log(listItems)
 }
+unorderedListElement.innerHTML = listItems
