1f2150cbcc2aae0fc2377637a2bddc5acb003389
adrianpaliz <adrianpaliz@gmail.com>
2024-04-15 22:49:10 -0500

    I have written a basic template string/literals

---
+++ javascript/build_a_chrome_extension/exercise_5/index.js
@@ -0,0 +1,7 @@
+const recipient = "James"
+
+//const email = "Hey " + recipient + "! How is it going? Cheers Per"
+
+const email = `Hey ${recipient}! How is it going? Cheers Per`
+console.log(email)
+
