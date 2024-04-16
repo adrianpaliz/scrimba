ba3908ff06f87395314c665b20d1d43992fffa62
adrianpaliz <adrianpaliz@gmail.com>
2024-04-15 20:15:34 -0500

    Remove hardcoded data, clean code and rearrange a line

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -1,17 +1,14 @@
-let myLeads =["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
-
+let myLeads =[]
 const inputElement = document.getElementById("input-element")
 const inputButtom = document.getElementById("input-button")
 const unorderedListElement = document.getElementById("unordered-list-element")
-let listItems = "" 
 
 inputButtom.addEventListener("click", function() {
 	myLeads.push(inputElement.value)
-	console.log(myLeads)
 })
 
+let listItems = ""
 for (let index = 0; index < myLeads.length; index++) {	
 	listItems += "<li>" + myLeads[index] + "</li>"
-	console.log(listItems)
 }
 unorderedListElement.innerHTML = listItems
