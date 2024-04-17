a0633044f866d64e63d868b3d5d87c1e35fe291d
adrianpaliz <adrianpaliz@gmail.com>
2024-04-17 06:40:11 -0500

    The leads has been saved to localStorage

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -1,15 +1,4 @@
-let myLeads = `["www.1337x.to"]`
-
-// 1. Turn the myLeads string into a array
-myLeads = JSON.parse(myLeads)
-// 2 Push a new value to the string
-myLeads.push("www.yts.mx")
-// 3 Turn the array into a string again
-myLeads = JSON.stringify(myLeads)
-// 4 Console.log the string using typeof to verify that it's a string
-console.log(typeof myLeads)
-
-
+let myLeads = []
 const inputElement = document.getElementById("input-element")
 const inputButtom = document.getElementById("input-button")
 const unorderedListElement = document.getElementById("unordered-list-element")
@@ -17,7 +6,10 @@ const unorderedListElement = document.getElementById("unordered-list-element")
 inputButtom.addEventListener("click", function() {
 	myLeads.push(inputElement.value)
 	inputElement.value = ""
-	renderLeads()
+	
+	localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+	
+	console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {
