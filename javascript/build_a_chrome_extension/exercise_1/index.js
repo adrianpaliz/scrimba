85ca84efc1bddd9a6e3ce2097d3cd74f2cd3e5fe
adrianpaliz <adrianpaliz@gmail.com>
2024-04-17 06:10:08 -0500

    Practice for convert a string into a array and a array into a string againt using parse and stringify methods

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -1,13 +1,18 @@
-let myLeads =[]
+let myLeads = `["www.1337x.to"]`
+
+// 1. Turn the myLeads string into a array
+myLeads = JSON.parse(myLeads)
+// 2 Push a new value to the string
+myLeads.push("www.yts.mx")
+// 3 Turn the array into a string again
+myLeads = JSON.stringify(myLeads)
+// 4 Console.log the string using typeof to verify that it's a string
+console.log(typeof myLeads)
+
+
 const inputElement = document.getElementById("input-element")
 const inputButtom = document.getElementById("input-button")
 const unorderedListElement = document.getElementById("unordered-list-element")
-
-//localStorage.setItem("mySite", "www.baidu.com")
-//localStorage.getItem("mySite")
-//let domain = localStorage.getItem("mySite")
-//console.log(domain)
-//localStorage.clear()
 
 inputButtom.addEventListener("click", function() {
 	myLeads.push(inputElement.value)
