c58bfdf04290e9c0c9a5752fcb58fedaa266ac5f
adrianpaliz <adrianpaliz@gmail.com>
2024-04-18 15:23:47 -0500

    Checking localStorage before rendering and call the renderLeads function

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -4,22 +4,23 @@ const inputButtom = document.getElementById("input-button")
 const unorderedListElement = document.getElementById("unordered-list-element")
 
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-console.log(leadsFromLocalStorage)
+
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage 
+	renderLeads()
+} 
 
 inputButtom.addEventListener("click", function() {
 	myLeads.push(inputElement.value)
 	inputElement.value = ""
 	localStorage.setItem("myLeads", JSON.stringify(myLeads) )
 	renderLeads()	
-
-	console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {
 	let listItems = ""
 	for (let index = 0; index < myLeads.length; index++) {
-	 // listItems += "<li><a target='_blank' href='" + myLeads[index] + "'>" + myLeads[index] + "</a></li>"
-		listItems += `
+	 	listItems += `
 			<li>
 				<a target="_blank" href="${myLeads[index]}">
 					${myLeads[index]}
