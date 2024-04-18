60c9490b2b54310f8b0bb808fe4d42368841a9a0
adrianpaliz <adrianpaliz@gmail.com>
2024-04-18 12:21:41 -0500

    Truthy and falsy values

---
+++ javascript/build_a_chrome_extension/exercise_6/index.js
@@ -0,0 +1,14 @@
+const credits = 0
+
+if (credits > 0) {
+	console.log("Let's play 🎰")
+} else {
+	console.log("Sorry, you have no credits 😭")
+}
+
+//false 
+//0
+//"" --> Emptness
+//null --> How you as a developer signalize emptiness
+//undefined --> How JavaScript signalizes emptiness
+//NaN --> It's a broken value
