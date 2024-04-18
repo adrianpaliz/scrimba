c2dba06131c2c84ab03ade1e381732190a1855b0
adrianpaliz <adrianpaliz@gmail.com>
2024-04-18 12:41:09 -0500

    Expressions to guess his truthy or falsy

---
+++ javascript/build_a_chrome_extension/exercise_7/index.js
@@ -0,0 +1,7 @@
+console.log( Boolean("")    ) // False
+console.log( Boolean("0")   ) // True
+console.log( Boolean(100)   ) // True
+console.log( Boolean(null)  ) // False
+console.log( Boolean([0] )  ) // True
+console.log( Boolean(-0)    ) // False
+
