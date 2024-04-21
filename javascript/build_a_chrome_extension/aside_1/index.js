f09451093d47916d41523d4140e8128fe04246a6
adrianpaliz <adrianpaliz@gmail.com>
2024-04-21 00:18:16 -0500

    Arguments vs Parameters

---
+++ javascript/build_a_chrome_extension/aside_1/index.js
@@ -0,0 +1,18 @@
+// What are greeting and name?   Parameters 
+// What are "Howdy" and "James"? Arguments 
+// What are num1 and num2?       Parameters
+// What are 3 and 4?             Arguments
+
+function greetUser(greeting, name) { 
+    welcomeEl.textContent = `${greeting}, ${name} 👋`
+}
+
+let hi = "Howdy"
+greetUser(hi, "James")
+
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+add(3, 4)
