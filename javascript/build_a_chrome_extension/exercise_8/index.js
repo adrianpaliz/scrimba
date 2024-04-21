47125377962fe42d0d9add151c3afd3d57ddca4b
adrianpaliz <adrianpaliz@gmail.com>
2024-04-20 23:19:15 -0500

    A function parameter was writter

---
+++ javascript/build_a_chrome_extension/exercise_8/index.js
@@ -0,0 +1,8 @@
+const welcomeElement = document.getElementById("welcome-element")
+
+function greetUser(greeting) {
+	welcomeElement.textContent = greeting + ", Adrián Páliz 👋"
+}
+
+greetUser("Alli puncha")
+
