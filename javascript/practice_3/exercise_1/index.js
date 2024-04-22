916137a33765e59cbf2fdee8cd1e5120dd827e1e
adrianpaliz <adrianpaliz@gmail.com>
2024-04-22 15:28:40 -0500

    Using let & const variables and template strings

---
+++ javascript/practice_3/exercise_1/index.js
@@ -0,0 +1,17 @@
+// Setting the stage
+const player = "Adrián"
+const opponent = "Isabel"
+const game = "AmazingFighter"
+let points = 0
+let hasWin = false
+
+// Playing the game
+points += 100
+hasWon = true
+
+// Announcing the winner
+if (hasWon) {console.log(`${player} got ${points} points and won the ${game} game!"` )
+} else {
+	console.log(`The winner is ${opponent}! ${palyer} lost the game`)
+}
+
