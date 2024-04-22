061ea5b6a176bb8f743cd0b93facc569e5d40b23
adrianpaliz <adrianpaliz@gmail.com>
2024-04-22 12:15:18 -0500

    Use the Chrome API to get the active URL tab

--- javascript/build_a_chrome_extension/exercise_1/index.js
+++ javascript/build_a_chrome_extension/exercise_1/index.js
@@ -11,15 +11,12 @@ if (leadsFromLocalStorage) {
 	render(myLeads)
 }
 
-const tabs = [
-	{url: "https://www.linkedin.com/in/adrianpaliz/"}
-]
-
 saveTabButton.addEventListener("click", function(){
-	myLeads.push(tabs[0].url)
-	localStorage.setItem("myLeads", JSON.stringify(myLeads))
-	render(myLeads)
-	
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		myLeads.push(tabs[0].url)
+		localStorage.setItem("myLeads", JSON.stringify(myLeads))
+		render(myLeads)	
+	})	
 })
 
 function render(leads) {
