4e28e72e7e20d50e05e47cf2e50c8e84d40e56e2
adrianpaliz <adrianpaliz@gmail.com>
2024-04-23 17:18:23 -0500

    Log out items in a array

---
+++ javascript/practice_3/exercise_2/index.js
@@ -0,0 +1,9 @@
+let myCourses = ["Learn CSS Animations", "UI Desing Fundamentals", "Intro to Clean Code"]
+
+function logItems(array) {
+	for (let index = 0; index < array.length; index++) {
+		console.log(array[index])		
+	}	
+}
+
+logItems(myCourses)
