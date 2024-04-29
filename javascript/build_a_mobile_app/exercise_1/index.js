f4e6c9160bd2e57ac599549939aa250cf0555f7b
adrianpaliz <adrianpaliz@gmail.com>
2024-04-29 17:28:38 -0500

    Firebase database was added to the project

--- javascript/build_a_mobile_app/exercise_1/index.js
+++ javascript/build_a_mobile_app/exercise_1/index.js
@@ -1,7 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js"
+
+const appSettings = {
+	databaseURL: ""
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDataBase = ref(database, "shoppingList")
+
 const inputFieldElement = document.getElementById("input-field")
 const addButtonElement = document.getElementById("add-button")
 
 addButtonElement.addEventListener("click", function(){
 	let inputValue = inputFieldElement.value
+	push(shoppingListInDataBase, inputValue)
 	console.log(inputValue)	
 })
