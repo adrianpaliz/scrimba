e7f94bf4691f8bc9f7d37f5b953612122cddee23
adrianpaliz <adrianpaliz@gmail.com>
2024-04-30 14:47:51 -0500

    Turning an Object into a array

---
+++ javascript/build_a_mobile_app/aside_2/index.js
@@ -0,0 +1,14 @@
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "per@scrimba.com",
+    "02": "frode@scrimba.com"
+}
+
+let scrimbaUsersEmails = Object.values(scrimbaUsers)
+console.log(scrimbaUsersEmails)
+
+let scrimbaUsersIds = Object.keys(scrimbaUsers)
+console.log(scrimbaUsersIds)
+
+let scrimbaUsersEntries = Object.entries(scrimbaUsers)
+console.log(scrimbaUsersEntries)
