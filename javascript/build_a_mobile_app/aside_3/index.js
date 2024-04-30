d8c48c2ec20eb71dfa93e14a16190040e7f346c6
adrianpaliz <adrianpaliz@gmail.com>
2024-04-30 15:54:32 -0500

    Fetching database items in realtime using onValue

---
+++ javascript/build_a_mobile_app/aside_3/index.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js"
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js"
+
+const appSettings = {
+    databaseURL: ""
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const booksInDataBase = ref(database, "books")
+
+const booksElement = document.getElementById("books")
+
+onValue(booksInDataBase, function(snapshot) {
+	let booksArray = Object.values(snapshot.val())
+	clearBooksListElement()
+	let listOfBooks = ""
+	for (let index = 0; index < booksArray.length; index++ ) {
+		let currentBook = booksArray[index]
+		appendBookToBooksListElement(currentBook)
+	}
+})
+
+function clearBooksListElement() {
+    booksElement.innerHTML = ""
+}
+
+function appendBookToBooksListElement(bookValue) {
+    booksElement.innerHTML += `<li>${bookValue}</li>`
+}
