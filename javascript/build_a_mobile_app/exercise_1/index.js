8cffc55c42fb99fe843e5230b13979d6217dc9c3
adrianpaliz <adrianpaliz@gmail.com>
2024-04-30 01:48:17 -0500

    Refactoring writting two new functions

--- javascript/build_a_mobile_app/exercise_1/index.js
+++ javascript/build_a_mobile_app/exercise_1/index.js
@@ -4,6 +4,7 @@ import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.11
 const appSettings = {
 	databaseURL: ""
 }
+
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDataBase = ref(database, "shoppingList")
@@ -15,7 +16,16 @@ const shoppingListElement = document.getElementById("shopping-list")
 addButtonElement.addEventListener("click", function(){
 	let inputValue = inputFieldElement.value
 	push(shoppingListInDataBase, inputValue)
-	inputFieldElement.value = ""
-	shoppingListElement.innerHTML += `<li>${inputValue}</li>`		
+
+	clearInputFieldElement()
+	appendItemToShoppingListElement(inputValue) 
 })
+
+function clearInputFieldElement() {
+	inputFieldElement.value = ""		
+}
+
+function appendItemToShoppingListElement(itemValue) {
+	shoppingListElement.innerHTML += `<li>${itemValue}</li>`
+}
 
