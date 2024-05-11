fe7ed9719bf09be678610bc86d9d307f145b4bc5
adrianpaliz <adrianpaliz@gmail.com>
2024-05-10 22:56:24 -0500

    Variables and datatypes

---
+++ python_101/exercises/variables_and_datatypes.py
@@ -0,0 +1,10 @@
+print("Variables & Datatypes - Exercise")
+#Create appropriate Varialble for Item name, the price
+#and how many you have in stock
+
+item_name = "Hose"
+price = 15.50
+inventory = 60
+is_in_inventory = True
+print(item_name, price, inventory)
+
