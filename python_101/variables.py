9f466d83c7ce9d809927de7169d3b5ae5799a981
adrianpaliz <adrianpaliz@gmail.com>
2024-05-10 22:16:27 -0500

    Using variables

---
+++ python_101/variables.py
@@ -0,0 +1,7 @@
+failed_subjects="2"
+name="Isaac"
+print("Dear MRs Páliz")
+print("Your son " + name + " is failing " + failed_subjects + " subjects.")
+print(name + " will need to redo " + failed_subjects + " courses")
+name="Adrián"
+print(name + " is doing well in geography")
