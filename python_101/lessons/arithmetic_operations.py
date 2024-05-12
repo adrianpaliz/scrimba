33d5bdd0cccb64dfd6f057814a0a07ff22cbe90c
adrianpaliz <adrianpaliz@gmail.com>
2024-05-11 22:38:57 -0500

    Arithmetic operations

---
+++ python_101/lessons/arithmetic_operations.py
@@ -0,0 +1,10 @@
+print("Basic Arithmetic")
+a = 10
+b = 3
+print("Addition : ", a + b)
+print("Subtraction : ", a - b)
+print("Multiplication : ", a * b)
+print("Division (float) : ", a / b)
+print("Modulus : ", a % b)
+print("Exponent : ", a ** b)
+
