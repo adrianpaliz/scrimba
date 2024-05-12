8032a590ad4fadfdb0dc996d55be29499356ae47
adrianpaliz <adrianpaliz@gmail.com>
2024-05-11 23:10:37 -0500

    Strings basics and Slicing

---
+++ python_101/lessons/strings_basics_slicing.py
@@ -0,0 +1,23 @@
+message = "welcome to Python 101: Strings"
+print("Strings basics")
+print(message+message)
+print(message*2)
+print(message,message)
+
+print(message.upper())
+print(message.lower())
+print(message.capitalize())
+print(message.title())
+
+print(len(message))
+print(message.count("o"))
+
+print("Slicing")
+print(message[0])
+print(message[5])
+print(message[-1])
+print(message[-3])
+print(message[2:])
+print(message[2:15])
+print(message[0:7])
+
