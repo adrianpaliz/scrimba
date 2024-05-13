37fd120ce36e388b15e384314d7f9b979ad01326
adrianpaliz <adrianpaliz@gmail.com>
2024-05-12 23:12:01 -0500

    User input

---
+++ python_101/lessons/user_input.py
@@ -0,0 +1,11 @@
+name = input("What is your name?: ")
+age = input("What is your age? ")
+print("Hello " + name + "! You are " + age + " years old")
+
+number_1 = input("Enter a digit: ")
+number_2 = input("Enter a second number: ")
+
+answer = float(number_1) + float(number_2)
+print(answer)
+
+
