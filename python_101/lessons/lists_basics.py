23b05b6f2c833b656dc64a8ae036962c463f4138
adrianpaliz <adrianpaliz@gmail.com>
2024-05-14 00:22:20 -0500

    Lists basics

---
+++ python_101/lessons/lists_basics.py
@@ -0,0 +1,14 @@
+print("Lists")
+
+items = ["Keys", "Wallet", "Smartphone", "Pen", "Notebook"]
+print(items)
+
+print(items[1])
+print(items[1],items[4])
+print(items[-1])
+print(items[2:4])
+print(items[:3])
+print(items[:])
+print(len(items))
+print(items.index("Smartphone"))
+print(items.count("Pen"))
