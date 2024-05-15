2641e1c29d0cbc6961e49045fa4acc654e03a1c8
adrianpaliz <adrianpaliz@gmail.com>
2024-05-15 07:15:26 -0500

    Tuples

---
+++ python_101/lessons/tuples.py
@@ -0,0 +1,12 @@
+friends_list = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+
+print(friends_list)
+print(friends_tuple)
+
+print(friends_list[2])
+print(friends_tuple[2])
+
+print(friends_list[2:4])
+print(friends_tuple[2:4])
+
