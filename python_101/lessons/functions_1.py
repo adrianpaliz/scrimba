7f9481480a6d32034997a59738312e0f9300b386
adrianpaliz <adrianpaliz@gmail.com>
2024-05-17 19:23:35 -0500

    Lesson about functions - part 1

---
+++ python_101/lessons/functions_1.py
@@ -0,0 +1,13 @@
+def greeting(name, age=27):
+    #print("Hello " + name + ", you are " + str(age) + "!")
+    print(f"Hello {name}, you are {age}!")
+
+name = input("Enter your name: ")
+
+greeting(name, "45")
+greeting("Judith")
+
+
+
+
+
