f511e787ccf95404f3fcf27bfb78e87295518f35
adrianpaliz <adrianpaliz@gmail.com>
2024-05-17 19:03:10 -0500

    Lesson about comments

---
+++ python_101/lessons/comments.py
@@ -0,0 +1,20 @@
+print("#Comments") 
+#Hiding in the comments
+# morecomments
+
+'''
+I'm also 
+learning to use 
+Tmux and Vim
+'''
+
+#fruits holds the fruits input by user as strings
+fruits = [‘apple’,’orange’,’banana’,’avocado’]
+
+#Entry form for Ministry applications
+#to-do: fix it! it doesn’t work  -Fixed
+name = "Default"
+name = input("Enter your silly name: ")
+print("Thank you " + name + "!")
+print("for applying to")
+print("the Minstry of Silly Walks")
