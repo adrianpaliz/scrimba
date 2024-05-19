5eae8700256d3014efd536ee9fec07928e5b836e
adrianpaliz <adrianpaliz@gmail.com>
2024-05-19 06:45:04 -0500

    Lesson about Comparisons and booleans

---
+++ python_101/lessons/comparisons_and_booleans.py
@@ -0,0 +1,57 @@
+a = 7 
+#Single equal is the asigment operator
+b = 3
+
+print(a == b) 
+#Two equal sings is the compare operator
+print(a != b)
+#Exclamation point plus equal is not equal
+print(a > b)
+#Greater than
+print(a < b)
+#Less than
+print(a <= b)
+#Less than or equal to
+print(a >= b)
+#Greater than or equal to
+print("o" in "John")
+#In operator denotes membership
+print("o" not in "John")
+#Non membership
+
+#Identity operator
+c = [3, 7, 42]
+d = c 
+print(c is d)
+print(id(c), id(d))
+
+e = [3, 7, 42]
+f = [3, 7, 42]
+print(e is f)
+print(id(e), id(f))
+print(e is not f)
+#Negative identity
+
+print(int(True))
+print(int(False))
+
+print(bool("Parrot"))
+print(bool(" "))
+print(bool(""))
+print(bool(42))
+print(bool(0))
+print(bool([1, 2]))
+print(bool([]))
+
+print(42 + True)
+print(42 + False)
+
+
+
+
+
+
+
+
+
+
