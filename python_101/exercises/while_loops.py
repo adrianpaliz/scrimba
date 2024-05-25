cfed801cd5beed66797b4ba28fc2d06499731794
adrianpaliz <adrianpaliz@gmail.com>
2024-05-24 20:58:25 -0500

    Basic teacher solution

--- python_101/exercises/while_loops.py
+++ python_101/exercises/while_loops.py
@@ -1,4 +1,5 @@
 #My solution
+'''
 print('Guessing game')
 attempts = 3
 number_to_guess = 69
@@ -16,4 +17,21 @@ while index < attempts:
             attempts += 2
         elif user_suggestion <= 50:
             print(f'You are low!')
+'''
+#Teacher solution
+number_to_guess = 12
+assumption_number = 0
+guess_limit = 3
+number_of_guesses = 0
+
+while number_of_guesses < guess_limit:
+    assumption_number = int(input(f'Guess # {number_of_guesses + 1} a number 1 - 20: last guess:{assumption_number} '))
+    if assumption_number == number_to_guess:
+        print(f'You Win! You guessed it: {assumption_number}')
+        break
+    else:
+        print(f'No, not {assumption_number}')
+        number_of_guesses += 1
+if assumption_number != number_to_guess:
+    print(f'Sorry you lose! it was {number_to_guess}')
 
