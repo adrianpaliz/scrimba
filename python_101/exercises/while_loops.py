14e27ddf77b5907d5a4788162b4f9fed743cd708
adrianpaliz <adrianpaliz@gmail.com>
2024-05-24 21:46:12 -0500

    Extended teacher solution

--- python_101/exercises/while_loops.py
+++ python_101/exercises/while_loops.py
@@ -19,19 +19,25 @@ while index < attempts:
             print(f'You are low!')
 '''
 #Teacher solution
-number_to_guess = 12
+number_to_guess = 76
 assumption_number = 0
-guess_limit = 3
+guess_limit = 5
 number_of_guesses = 0
 
+assumption_number = int(input(f'Guess a number 1 - 100: '))
+number_of_guesses += 1
 while number_of_guesses < guess_limit:
-    assumption_number = int(input(f'Guess # {number_of_guesses + 1} a number 1 - 20: last guess:{assumption_number} '))
+    
+    if assumption_number != number_to_guess:
+        number_of_guesses += 1
+        if assumption_number > number_to_guess:
+            assumption_number = int(input(f'{assumption_number} too high - Guess again 1 - 100: '))
+        else:
+            assumption_number = int(input(f'{assumption_number} too low - Guess again 1 - 100: '))
     if assumption_number == number_to_guess:
         print(f'You Win! You guessed it: {assumption_number}')
         break
-    else:
-        print(f'No, not {assumption_number}')
-        number_of_guesses += 1
+
 if assumption_number != number_to_guess:
     print(f'Sorry you lose! it was {number_to_guess}')
 
