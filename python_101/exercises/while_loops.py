5e21040701865770b0745d5a48593b1e90c2bfcc
adrianpaliz <adrianpaliz@gmail.com>
2024-05-24 20:21:16 -0500

    My solution to the while loops exercise

--- python_101/exercises/while_loops.py
+++ python_101/exercises/while_loops.py
@@ -1,11 +1,19 @@
+#My solution
 print('Guessing game')
-
-number_to_guess = 3
+attempts = 3
+number_to_guess = 69
 index = 0
-while index < 3 :
-    index += 1
+while index < attempts:
     user_suggestion = int(input('Guess the number, type your beat: '))
+    index += 1
     if user_suggestion == number_to_guess:
         print(f'You guessed it')
+        exit()
     else:
         print(f'You didn\'t guessed it')
+        if user_suggestion >= 50:
+            print(f'You are close! you win 2 more chances')
+            attempts += 2
+        elif user_suggestion <= 50:
+            print(f'You are low!')
+
