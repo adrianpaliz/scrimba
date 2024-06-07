c200772b806de0fddd105d75cd1940fa17df8c58
adrianpaliz <adrianpaliz@gmail.com>
2024-06-07 16:06:50 -0500

    Implementation of try and except

--- python_101/lessons/try_except_raise.py
+++ python_101/lessons/try_except_raise.py
@@ -1,4 +1,7 @@
-number = int(input('Enter a number: '))
-print('30 divided by', number, 'is: ', 30/number)
+try:
+    number = int(input('Enter a number: '))
+    print('30 divided by', number, 'is: ', 30/number)
+except:
+    print('Invalid Input!')
 print('**Thank you for playing!**')
 
