42967a54bf5fe4ec7a8c83ec52faf2412b078028
adrianpaliz <adrianpaliz@gmail.com>
2024-06-07 16:44:03 -0500

    Lesson about try or except, raice exceptions

--- python_101/lessons/try_except_raise.py
+++ python_101/lessons/try_except_raise.py
@@ -1,7 +1,21 @@
 try:
-    number = int(input('Enter a number: '))
-    print('30 divided by', number, 'is: ', 30/number)
+    number = int(input('Enter a number between 1 and 30: '))
+    number_1 = 30 / number
+    if number > 30:
+        raise ValueError(number)
+
+except ZeroDivisionError as err:
+    print(err, "You can't devide by zero!")
+
+except ValueError as err:
+    print(err, number, 'Bad Value! not between 1 and 30!')
+    
 except:
     print('Invalid Input!')
-print('**Thank you for playing!**')
+
+else:
+    print('30 divided by', number, 'is: ', 30/number)
+
+finally:
+    print('**Thank you for playing!**')
 
