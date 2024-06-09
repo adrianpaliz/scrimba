3c19db9b9f343698e1ca34cda2309f27ced05e76
adrianpaliz <adrianpaliz@gmail.com>
2024-06-09 09:42:10 -0500

    Lesson about modules

---
+++ python_101/lessons/modules.py
@@ -0,0 +1,13 @@
+#import platform
+#import string, os
+#import platform as pl 
+#from platform import python_version, system
+from platform import python_version as pv
+
+#print(dir(platform))
+#print(platform.python_version())
+#print(pl.python_version())
+#print(python_version())
+#print(system())
+print(pv())
+
