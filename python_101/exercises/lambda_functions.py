1175ed76a62c162fc6168e981696828183e8cb61
adrianpaliz <adrianpaliz@gmail.com>
2024-06-20 23:09:21 -0500

    Sixth exercise's solution

--- python_101/exercises/lambda_functions.py
+++ python_101/exercises/lambda_functions.py
@@ -38,10 +38,26 @@ print(f(2))
 print(g(2))
 '''
 #Fifth exercise
+'''
 signups = ['MPF104', 'MPF20', 'MPF2', 'MPF17', 'MPF3', 'MPF45']
 print(sorted(signups)) # Lexicographic sort
 #My solution
-print(sorted(signups,  key = lambda signups: int(''.join(filter(str.isdigit, signups))))) # Integer sort
+print(sorted(signups, key = lambda signups: int(''.join(filter(str.isdigit, signups))))) # Integer sort
 #Teacher solution
 print(sorted(signups, key = lambda id: int(id[3:]))) 
+'''
+#Sixth exercise
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+Ricardo = Player('Ricardo', 116700)
+Karen = Player('Karen', 24327)
+Ernesto = Player('Ernesto', 150000)
+
+player_list = [Ricardo, Karen, Ernesto]
+player_list.sort(key = lambda player_score: player_score.score) 
+
+print([player.name for player in player_list])
 
