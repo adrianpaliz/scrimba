554d5f5187f24f6936e7aed916e78732b8497f09
adrianpaliz <adrianpaliz@gmail.com>
2024-07-09 19:50:58 -0500

    We made diferent approaches to the project looking for efficiency

--- python_101/projects/euler_palindromes.py
+++ python_101/projects/euler_palindromes.py
@@ -66,5 +66,60 @@ def palindrome_back():
     print('Runtime:', time.time() - start_time)
     print('---------End run---------')
 
-palindrome()
-palindrome_back()
+def palindrome_create():
+    numbers=('9','8','7','6','5','4','3','2','1','0') # we create a list of the strings we will use
+    iterations = 0 # we bring along our trusty iterator
+    
+    for digit_1 in range(10):
+        for digit_2 in range(10):
+            for digit_3 in range(10):
+                #we create the palindrome string by slicing using the iterators
+                palindrome_string = numbers[digit_1] + numbers[digit_2] + numbers[digit_3] + numbers[digit_3] + numbers[digit_2] +numbers[digit_1]  
+                palindrome = int(palindrome_string) # typecast string into an integer
+                # print(pal_str, palindrome) #debug to check palindromes tested
+                
+                #lowest number to check against that can generate a higher 
+                low_value = int(palindrome / 999) # or //999 floor div gives int as answer 
+                # highest possible factor to check, one factor must be lower than this
+                high_value = int(palindrome ** 0.5) + 1 # **0.5 is "the square root" of palindrome 
+                
+                # check if palindrome divisible by any of the numbers between min and max
+                for digit in range(low_value, high_value):
+                    iterations += 1
+                    if palindrome % digit == 0: #check for divisibility, since we are stepping through palindromes in order, as soon as we find one: we are Done! 
+                       
+                        return 'palindrome:', palindrome, 'digit:',digit, 'palindrome / digit:', 
+                        palindrome / digit ,'iterations:',iterations #gives nicer close out of loops
+
+def palindrome_create_2():
+    
+    iterations = 0 # our trusty iterator
+    
+    for digit_1 in range(9, 0, -1):
+        for digit_2 in range(9, -1, -1):
+            for digit_3 in range(9, -1, -1):
+                # we create the -palindromes by adding the 'placevalues', 
+                # example step 999999 -> 998899 -> 997799 etc but only to 100001 
+                palindrome = digit_1 * 100000 + digit_2 * 10000 + digit_3 * 1000 + digit_3 * 100 + digit_2 * 10 + digit_1
+                #print(palindrome) #debug
+                
+                low_value = int(palindrome / 999) # or low_val = palindrome//999 
+                high_value = int(palindrome ** 0.5) + 1
+                for digit in range(low_value, high_value):
+                    iterations += 1
+                    if palindrome % digit == 0:
+                        
+                        return 'palindrome:', palindrome, 'digit:', digit, 'palindrome / digit:', 
+                        palindrome / digit, 'iterations:', iterations
+
+
+#palindrome()
+#palindrome_back()
+for function in ['palindrome_create', 'palindrome_create_2']:
+    runs = 10
+    start = time.time()
+    for run in range(runs):
+        return_value = locals()[function]()
+        end = time.time()
+    print(return_value, 'Average run-time:', (end - start) / runs)
+
